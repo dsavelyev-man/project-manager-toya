6aaa1b9694987d0a61a28a125299be0656450c7a
dsavelyev <kriper0nind@gmail.com>
2024-01-08 00:57:23 +0300

    test

--- web/src/helpers/getOwnerProject.ts
+++ web/src/helpers/getOwnerProject.ts
@@ -1,7 +1,5 @@
 import { ProjectWithMembers } from "@/api/projects.ts";
-import { PROJECT_ROLES } from "shared";
-
-console.log(PROJECT_ROLES);
+// import { PROJECT_ROLES } from "shared";
 
 const getOwnerProject = (project: ProjectWithMembers) => {
   let owner = project.members.find((item) => item.role === "OWNER");
